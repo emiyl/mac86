9eee5b4c907593f31741102aa9e24580ae95acc1
emiyl <me@emiyl.com>
2026-05-18 03:25:59 +0100

    remove unnecessary binaries

--- samples/phase1_main.c
+++
@@ -1,7 +0,0 @@
-#include "syscall.h"
-
-int main(void) {
-    static const char msg[] = "hello from freestanding c\n";
-    mac86_sys_write(1, msg, (unsigned int)(sizeof(msg) - 1));
-    return 0;
-}
